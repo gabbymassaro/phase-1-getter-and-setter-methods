421807e20d561a35a6d425503d31d0c6879e747c
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-05 13:48:23 -0400

    All the gets work, none of the sets are working

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this.radius = radius
+  }
+
+  get diameter() {
+    return this.radius * 2
+  }
+
+  set diameter(diameter) {
+    this.radius = diameter * 2
+  }
+
+  get circumference() {
+    return this.diameter * Math.PI
+  }
+
+  set circumference(circumference) {
+    this.radius = (circumference * Math.PI)
+  }
+
+  get area() {
+    return (this.radius * this.radius) * Math.PI
+  }
+
+  set area(area) {
+    this.radius = (Math.sqrt(area)) * Math.PI
+  }
+
+}
+
+let test = new Circle(6)
+console.log("Radius: ", test.radius)
+console.log("Diameter: ", test.diameter)
+console.log("Circumference: ", test.circumference)
+console.log("Area: ", test.area)
